23a93267790f7344694a253680a022713da3e0ed
maoudev <renatocavieres4321@gmail.com>
2023-12-01 23:24:07 -0300

    configure pacakge json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from "@astrojs/netlify/functions";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: netlify(),
+  adapter: node({
+    mode: "standalone"
+  })
 });
